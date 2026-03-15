f7f779efebe570702d27787694038a0fe055648a
Timothy Miller <innerlogic4321@gmail.com>
2026-03-15 12:50:07 +0900

    Add Avalonia sample

---
+++ samples/BSkyOAuth/BSkyOAuthAvalonia/BSkyOAuthAvalonia.Desktop/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using Avalonia;
+using BSkyOAuthAvalonia;
+
+namespace BSkyOAuthAvalonia.Desktop;
+
+internal class Program
+{
+    [STAThread]
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
+
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .LogToTrace();
+}
