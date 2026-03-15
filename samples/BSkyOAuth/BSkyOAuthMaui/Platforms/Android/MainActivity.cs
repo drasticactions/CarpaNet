32911f8d0747f4bbf886f955420b52501eacf773
Timothy Miller <innerlogic4321@gmail.com>
2026-03-15 12:49:55 +0900

    Maui samples

--- samples/BSkyOAuth/BSkyOAuthMaui/Platforms/Android/MainActivity.cs
+++ samples/BSkyOAuth/BSkyOAuthMaui/Platforms/Android/MainActivity.cs
@@ -1,4 +1,5 @@
-﻿using Android.App;
+using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.OS;
 
@@ -6,5 +7,14 @@ namespace BSkyOAuthMaui;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
+{
+}
+
+[Activity(NoHistory = true, LaunchMode = LaunchMode.SingleTop, Exported = true)]
+[IntentFilter(
+    [Intent.ActionView],
+    Categories = [Intent.CategoryDefault, Intent.CategoryBrowsable],
+    DataScheme = "vip.drasticactions")]
+public class WebAuthenticatorCallbackActivity : Microsoft.Maui.Authentication.WebAuthenticatorCallbackActivity
 {
 }
